94f9fba59915a82844592f43d9a57e3f39d35894
dismay <93299449+Devilish-Trio@users.noreply.github.com>
2023-06-02 15:40:27 +1000

    yeh

---
+++ SQLConnectionTester/main.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='<database name>',
+                                         user='root',
+                                         password='<password>')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM ratings LIMIT 10;")  # replace 'your_table' with the name of your table
+
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+finally:
+    if connection is not None and connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
